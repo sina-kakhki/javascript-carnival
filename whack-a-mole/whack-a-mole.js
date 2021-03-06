95465234780c5e52045d153de7c24341bc7e03dc
Sina-Kakhki <ksina.72@gmail.com>
2021-03-07 01:33:52 +1300

    finished whack-a-mole challenge

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -2,4 +2,23 @@
  // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+var cells = document.getElementsByTagName("TD") //grabs each table data cell individually
+
+var randomIndex = Math.floor(Math.random() * cells.length)  //picks a random cell depending on the length of the cells variable
+
+var randomCell = cells[randomIndex]   
+
+var mole = document.createElement('img');
+mole.src = './mole.PNG'
+mole.id = "mole"
+randomCell.appendChild(mole);
+
+mole.onclick = whackedMole
+
+function whackedMole() {
+  randomIndex = Math.floor(Math.random() * cells.length)
+  randomCell = cells[randomIndex]
+  randomCell.appendChild(mole);
+  var audio = new Audio("whack-audio.wav");
+  audio.play();  
+}
